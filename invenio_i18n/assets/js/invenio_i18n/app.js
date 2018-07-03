7de96bc096646e99069a9f783f8ca40c38680b10
ioanungurean <inungurean@gmail.com>
2018-07-03 16:54:14 +0200

    global: initial webpack support

---
+++ invenio_i18n/assets/js/invenio_i18n/app.js
@@ -0,0 +1,26 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2016-2018 CERN.
+ * Copyright (C) 2016 TIND.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+import angular from 'angular';
+import 'angular-gettext/dist/angular-gettext';
+import 'jquery/dist/jquery';
+
+
+angular.module("langSelector", ["gettext"])
+  .factory("setLanguage", ["gettextCatalog", function (gettextCatalog) {
+    function setCurrentLanguage(lang) {
+      gettextCatalog.setCurrentLanguage(lang);
+    }
+  }]);
+
+$(document).ready(function () {
+  $('#lang-code').on('change', function () {
+    $('#language-code-form').submit();
+  });
+});

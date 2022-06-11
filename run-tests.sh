e6cafef1ac7a968e92a9c6648c3e296ae6c6634d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-06-12 00:12:52 +0200

    move check_manifest configuration to setup.cfg.
    concentrate the configuration of all calls in one place

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -14,7 +15,7 @@ set -o nounset
 
 python -m setup compile_catalog
 python -m babel.messages.frontend compile -d tests/translations/
-python -m check_manifest --ignore ".*-requirements.txt"
+python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
 python -m pytest

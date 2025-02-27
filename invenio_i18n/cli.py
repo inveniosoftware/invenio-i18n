2f354917f2b3ad6cb041a0bd78cee0f58aaee95d
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2025-02-27 14:50:39 +0100

    Translation distribution: Add command to download translations from transifex and combine them into a single file

--- invenio_i18n/cli.py
+++ invenio_i18n/cli.py
@@ -8,13 +8,16 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """CLI for Invenio internationalization module"""
-import click
 import json
+import subprocess
 from importlib.metadata import entry_points
 from pathlib import Path
 
-from flask.cli import with_appcontext
+import click
+import polib
 from flask import current_app
+from flask.cli import with_appcontext
+from jinja2 import BaseLoader, Environment
 
 
 @click.group(chain=True)
@@ -52,7 +55,7 @@ def distribute_js_translations(input_directory: Path, entrypoint_group: str):
         exceptional_package_name_mapper.get(
             bundle_entrypoint.name, bundle_entrypoint.name
         ): Path(bundle_entrypoint.load().path)
-        / "translations"
+           / "translations"
         for bundle_entrypoint in bundle_entrypoints
         if bundle_entrypoint.load().path
     }
@@ -91,7 +94,7 @@ def distribute_js_translations(input_directory: Path, entrypoint_group: str):
                 target_translations_path.mkdir(parents=True)
 
             with (target_translations_path / "translations.json").open(
-                "w"
+                    "w"
             ) as target_file:
                 json.dump(translations, target_file, indent=2, ensure_ascii=False)
 
@@ -99,3 +102,90 @@ def distribute_js_translations(input_directory: Path, entrypoint_group: str):
                 f"{package_name} translations for language {language} have been written.",
                 fg="green",
             )
+
+
+@i18n.command()
+@click.option(
+    "--token", "-t", required=True, help="API token for your Transifex account."
+)
+@click.option(
+    "--languages",
+    "-l",
+    required=True,
+    help="Languages you want to download translations for (one or multiple comma separated values).",
+)
+@click.option(
+    "--output-directory",
+    "-o",
+    required=True,
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, writable=True, path_type=Path
+    ),
+    help="Directory to which collected translations in JSON format should be written.",
+)
+def download_transifex(token, languages, output_directory):
+    """Compile message catalog."""
+    js_resources = current_app.config["I18N_TRANSIFEX_JS_RESOURCES_MAP"]
+
+    downloads_path = output_directory / "download_transifex"
+    downloads_path.mkdir(parents=True, exist_ok=True)
+
+    with open(downloads_path / "collected_config", "w") as f:
+        # TODO Transifex downloads to path in config, change config if a different file location for *.po files is needed
+        config_template = Environment(loader=BaseLoader()).from_string("""
+[main]
+host = https://www.transifex.com
+
+[o:inveniosoftware:p:invenio:r:{{- resource }}]
+file_filter = {{- downloads_path }}/{{- module }}/<lang>/messages.po
+source_file = {{- module }}/assets/semantic-ui/translations/{{- module }}/translations.pot
+source_lang = en
+type = PO
+        """)
+
+        for resource, module in js_resources.items():
+            f.write(
+                config_template.render(
+                    downloads_path=downloads_path, resource=resource, module=module
+                )
+            )
+            f.write("\n\n")
+
+    transifex_pull_cmd = [
+        "tx",
+        f"--token={token}",
+        f"--config={downloads_path}/collected_config",
+        "pull",
+        f"--languages={languages}",
+        "--force",
+    ]
+    subprocess.run(transifex_pull_cmd)
+
+    collected_translations = {}
+
+    languages_array = languages.split(",")
+    for language in languages_array:
+        collected_translations[language] = {}
+
+        for resource, module in js_resources.items():
+            po_path = f"{downloads_path}/{module}/{language}/messages.po"
+
+            pofile = polib.pofile(po_path)
+            collected_translations[language][module] = {}
+
+            for entry in pofile:
+                collected_translations[language][module][entry.msgid] = entry.msgstr
+                if entry.msgstr_plural:
+                    collected_translations[language][module][entry.msgid] = (
+                        entry.msgstr_plural[0]
+                    )
+                    collected_translations[language][module][
+                        entry.msgid + "_plural"
+                        ] = entry.msgstr_plural[1]
+
+        with open(
+                f"{output_directory}/{language}.json",
+                "w",
+                encoding="utf-8",
+        ) as f:
+            json.dump(collected_translations[language], f, indent=4, ensure_ascii=False)

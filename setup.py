d35e173f5882963e3744492e2cf28e018ec269ea
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 12:49:23 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2015-2018 CERN.
-# SPDX-FileCopyrightText: 2016 TIND.
-# SPDX-License-Identifier: MIT
-
-"""Invenio module to manage internationalization and localization."""
-
-from setuptools import setup
-
-setup()

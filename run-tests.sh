3f2519279deb47d005f71f6b3ac2ab4d66148e41
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 12:49:23 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2015-2018 CERN.
 # SPDX-FileCopyrightText: 2022 Graz University of Technology.
+# SPDX-FileCopyrightText: 2026 TU Wien.
 # SPDX-License-Identifier: MIT
 
 # Quit on errors
@@ -8,8 +9,7 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
-python -m setup compile_catalog
+pybabel compile -d invenio_i18n/translations --use-fuzzy --output-file /dev/null
 python -m babel.messages.frontend compile -d tests/translations/
-python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 python -m pytest
